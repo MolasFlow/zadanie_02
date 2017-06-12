5aa79855a26711aa0a58736337355003b5141fb3
Bartosz Marcol <b.marcol@op.pl>
2017-06-12 13:37:29 +0200

    makeing test and comments in js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,15 @@
 // scripts.js
+// Below is the command that makeing the test of JavaScripts working
+var variable = 'test';
+
+console.log(variable); // I don't actually understant how it works
+
 
 var name = prompt('Enter your name');
 alert('Hello, ' + name);
-console.log('Hello, ' + name);
+console.log('Hello, ' + name);   
+
+/*
+  and there is an example of block comment
+*/
+
