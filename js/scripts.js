c41ba4a0af3fd17a66e2a9f1b11d7b5decab17a4
Bartosz Marcol <b.marcol@op.pl>
2017-06-14 14:06:03 +0200

    add DS_Store to .gitignore and repair the js code

--- js/scripts.js
+++ js/scripts.js
@@ -15,6 +15,6 @@ console.log('Hello, ' + name);
 
 var a = 12,
 	h = 8,
-	triangleArea = a*h/2;
+	triangleArea = a * h / 2;
 
 console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
