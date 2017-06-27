4f09827deb2281adb75aadb6e0659484ac1d1057
Bartosz Marcol <b.marcol@op.pl>
2017-06-27 17:36:04 +0200

    doing the strings

--- js/scripts.js
+++ js/scripts.js
@@ -52,6 +52,17 @@ if (allNames.indexOf(newName) === -1) {
 
 console.log(allNames);
 
+var text = 'Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.';
+var dinosaur = 'triceratops'
 
+var dinosaurUpperCased = dinosaur.toUpperCase();
+
+var textCharsAfter = text.replace('Velociraptor', dinosaurUpperCased);
+
+var length = text.length
+
+var partOftext = textCharsAfter.slice(0,length/2);
+
+console.log( partOftext )
 
 
