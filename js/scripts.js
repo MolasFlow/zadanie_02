6055eb495c629fbff16fe5b04c8b13a459b2e506
Bartosz Marcol <b.marcol@op.pl>
2017-06-28 11:57:05 +0200

    reapired the example 9.1

--- js/scripts.js
+++ js/scripts.js
@@ -4,39 +4,39 @@ var variable = 'test';
 
 console.log(variable); // ok i understand now
 
-
+/*
 var name = prompt('Enter your name');
 alert('Hello, ' + name);
 console.log('Hello, ' + name);   
+*/
 
 /*
   and there is an example of block comment
 */
 
 function getTriangleArea(a, h) {
-	if (a <= 0) {
-		console.log('Nieprawidłowe dane')
-	} else if (h <= 0) {
+	if (a <= 0 || h <= 0) {
 		console.log('Nieprawidłowe dane')
 	} else {
-	return (a * h / 2)
+		return a * h / 2
 	}
-}
+};
 
-console.log( getTriangleArea(10,6) )
+console.log(getTriangleArea(10, 6));
 
 var triangle1Area = getTriangleArea(10, 15);
 
-console.log( triangle1Area )
+console.log(triangle1Area);
 
-var triangle2Area = getTriangleArea( - 3, 15);
+var triangle2Area = getTriangleArea(-3, 15);
 
-console.log( triangle2Area )
+console.log(triangle2Area);
 
 var triangle3Area = getTriangleArea(2, 0);
 
-console.log( triangle3Area )
+console.log(triangle3Area);
 
+/*
 var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
 var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
 
@@ -64,5 +64,5 @@ var length = text.length
 var partOftext = textCharsAfter.slice(0,length/2);
 
 console.log( partOftext )
-
+*/
 
