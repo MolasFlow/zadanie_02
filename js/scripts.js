a9f44589826c7b76b007f31557451d2786ac7bf1
Bartosz Marcol <b.marcol@op.pl>
2017-08-21 13:02:16 +0200

    firstcommit

--- js/scripts.js
+++ js/scripts.js
@@ -1,138 +1,76 @@
-// scripts.js
-// Below is the command that makeing the test of JavaScripts working
-var variable = 'test';
-
-console.log(variable); // ok i understand now
-
-/*
-var name = prompt('Enter your name');
-alert('Hello, ' + name);
-console.log('Hello, ' + name);   
 
 
 
 
-function getTriangleArea(a, h) {
-	if (a <= 0 || h <= 0) {
-		console.log('Nieprawidłowe dane');
+
+var input = document.getElementById("myInput");
+var p = document.getElementById("output");
+
+var firtValue = 20,
+	secondValue = 70,
+	thirdValue = 130,
+	fourthValue = 180, 
+	fifthValue = 200;
+
+function changetax($this){
+	if ($this.value > 100 && $this.value <= 1000) {
+ 		$("#output").text("Wysokość składki: " + firtValue + " zł");
+	} else if ($this.value > 1000 && $this.value <= 3000) {
+		$("#output").text("Wysokość składki: " + secondValue + " zł");
+	} else if ($this.value > 3000 && $this.value <= 6000) {
+		$("#output").text("Wysokość składki: " + thirdValue + " zł");
+	} else if ($this.value > 6000 && $this.value <= 9000) {
+		$("#output").text("Wysokość składki: " + fourthValue + " zł");
+	} else if ($this.value > 9000 && $this.value <= 20000) {
+		$("#output").text("Wysokość składki: " + fifthValue + " zł");
+	} else if ($this.value < 100 ) {
+		$("#output").text("Wysokość składki: wartość jest za niska");
+	} else if ($this.value > 20000) {
+		$("#output").text("Wysokość składki: wartość jest za wysoka");
 	} else {
-		return a * h / 2;
+		$("#output").text("Wysokość składki:");
 	}
-};
 
-console.log(getTriangleArea(10, 6));
-
-var triangle1Area = getTriangleArea(10, 15);
-
-console.log(triangle1Area);
-
-var triangle2Area = getTriangleArea(-3, 15);
-
-console.log(triangle2Area);
-
-var triangle3Area = getTriangleArea(2, 0);
-
-console.log(triangle3Area);
-
-
-var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
-var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
-
-var allNames = femaleNames.concat(maleNames);
-
-var newName = 'Marian';
-
-if (allNames.indexOf(newName) === -1) {
-	allNames.push(newName);
-} else {
-	console.log('To imie jest już dodane');
-};
-
-console.log(allNames);
-
-
-var text = 'Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.';
-var dinosaur = 'triceratops';
-
-var dinosaurUpperCased = dinosaur.toUpperCase();
-
-var textCharsAfter = text.replace('Velociraptor', dinosaurUpperCased);
-
-var length = text.length;
-
-var partOftext = textCharsAfter.slice(0, length/2);
-
-console.log(partOftext);
-
-
-function drawTree(height) {
-	withButtonItemsClass
-		for (var j = 0; j < height - i; j++) {
-			star += ' ';
-		}
-		for (var j = 0; j <= i; j++) {
-			star += '* '; // dodałem spację po *, żeby choinka stała prosto :)
-		}
-		console.log(star);	
-	}
 }
 
-drawTree(5);
+changetax(1);
+/////poniżej próbuje uruchomić checkboxy/////
+/*
+var changeAmound = $("#output").text($this.value>0?
 
-
-
-var withButtonItemsClass = document.getElementsByClassName('button');
-
-console.log(withButtonItemsClass);
-
-var buttonLenght = withButtonItemsClass.length;
-
-var newElem = withButtonItemsClass;
-
-for (var b = 0; b < buttonLenght; b++) {
-	alert(newElem[b].innerText);
+function newAmound() {
+	var included = $('#box-1').is(':checked');
+		if (included) {
+			$("#output").text(firstValue * 0,95);		
+	}
 }
 */
 
 
-var list = document.getElementById('list');
 
-var add = document.getElementById('addElem');
-
-var itemNumber = document.getElementsByTagName('li');
-
-add.addEventListener('click', function() {
-	var element = document.createElement('li');
-	element.innerHTML = 'item ' + itemNumber.length + ' '; 
-	list.appendChild(element);
-
+/*
+///////this is my draft only///////////////////////////
+input.addEventListener('input', function() {
+  var inputText = this.value; // this.value przechowuje aktualny tekst z inputa
+  p.innerText = "Wysokość składki:" + inputText;
 });
 
+$(document).ready(function(){ 
+var val = $( "#input" ).val();
+if (val == 100) {
+        $( "#output" ).text("20zł");
+    }
+}); 
 
+function setTheValue() {
+	if (this.value <= 0) {
+		p.innerText = "Wysokość składki:" + "za niska wartość";
+	} else if (this.value >= 100) {
+		p.innerText = "Wysokość składki:" + "20zł";	
+	} else {
+		return "wartosc zbyt wysoka"
+	}
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+setTheValue;
+*/
